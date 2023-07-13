c494b4052c0e99b34b2e7bc69be8d53684b8464b
Pious <pioushasan2305@gmail.com>
2023-07-14 00:26:49 +0600

    added all possible permutations for a t value

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 import itertools
 import tuscan
 
-def permutations(n):
+def permutations(n, t=None):
     numbers = list(range(n))
-    permutations = itertools.permutations(numbers)
+    permutations = itertools.permutations(numbers, t)
     return permutations
 
-n = int(input("Enter the number of test orders: "))
+n = int(input("Enter the number of tests: "))
 
-print("Total permutations of test orders: ")
+print("Total Possible permutations of tests: ")
 total_permutations = permutations(n)
 for permutation in total_permutations:
     print(permutation)
 
+t = int(input("Enter t value: "))
+
+print("Total Possible permutations of length t: ")
+total_permutations_t = permutations(n, t)
+for permutation_t in total_permutations_t:
+    print(permutation_t)
+
+# Assuming 'generate_tuscan_permutations' is a function to generate the Tuscan permutations
+# Replace this with the actual function name if it's different
 print("Prioritized permutations of test orders: ")
 prioritized_permutations = tuscan.generate_tuscan_permutations(n)
 for permutation in prioritized_permutations:
