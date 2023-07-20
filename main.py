74544fb9f905c0c48b87dd3912589f1862dfc227
Satvik Eltepu <satvik09@hotmail.com>
2023-07-20 01:10:08 -0700

    Tuscan Inter-Class algorithm for 2 classes

--- main.py
+++ main.py
@@ -1,26 +1,49 @@
 import itertools
 import tuscan
 
-def permutations(n, t=None):
-    numbers = list(range(n))
-    permutations = itertools.permutations(numbers, t)
-    return permutations
+num_classes = int(input("Enter the number of classes (1 or 2): "))
 
-n = int(input("Enter the number of tests: "))
+def one_class():
+    def permutations(n, t=None):
+        numbers = list(range(n))
+        permutations = itertools.permutations(numbers, t)
+        return permutations
 
-print("Total Possible permutations of tests: ")
-total_permutations = permutations(n)
-for permutation in total_permutations:
-    print(permutation)
+    n = int(input("Enter the number of tests: "))
 
-t = int(input("Enter t value: "))
+    print("Total Possible permutations of tests: ")
+    total_permutations = permutations(n)
+    for permutation in total_permutations:
+        print(permutation)
 
-print("Total Possible permutations of length t: ")
-total_permutations_t = permutations(n, t)
-for permutation_t in total_permutations_t:
-    print(permutation_t)
+    t = int(input("Enter t value: "))
 
-print("Prioritized permutations of test orders: ")
-prioritized_permutations = tuscan.generate_tuscan_permutations(n)
-for permutation in prioritized_permutations:
-    print(permutation)
+    print("Total Possible permutations of length t: ")
+    total_permutations_t = permutations(n, t)
+    for permutation_t in total_permutations_t:
+        print(permutation_t)
+
+    print("Prioritized permutations of test orders: ")
+    prioritized_permutations = tuscan.generate_tuscan_permutations(n)
+    for permutation in prioritized_permutations:
+        print(permutation)
+
+def multi_class():
+    methods_per_class = []
+    for i in range(2):
+        methods = int(input("How many methods are in class " + str(i) + ": "))
+        methods_per_class.append(methods)
+    class_one_tuscan_permutations = tuscan.generate_tuscan_permutations(methods_per_class[0])
+    class_two_tuscan_permutations = tuscan.generate_tuscan_permutations(methods_per_class[1])
+    print("Prioritized permutations of test orders: ")
+    for i in range(len(class_two_tuscan_permutations)):
+        for j in range(len(class_one_tuscan_permutations)):
+            print("Class 1: " + str(class_one_tuscan_permutations[j]) + "; Class 2: " + str(class_two_tuscan_permutations[i]))
+    for i in range(len(class_one_tuscan_permutations)):
+        for j in range(len(class_two_tuscan_permutations)):
+            print("Class 2: " + str(class_two_tuscan_permutations[j]) + "; Class 1: " + str(class_one_tuscan_permutations[i]))      
+
+if num_classes == 1:
+    one_class()
+else:
+    multi_class()
